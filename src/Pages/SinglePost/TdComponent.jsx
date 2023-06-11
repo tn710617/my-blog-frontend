83c2e3999f4ad056d3be02d6e85c4bd4378df65e
Ray <tn710617@gmail.com>
2023-06-11 19:07:49 +0800

    [fix] fixed incorrect td style

--- src/Pages/SinglePost/TdComponent.jsx
+++ src/Pages/SinglePost/TdComponent.jsx
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function TdComponent(props) {
     return (
-        <td className="border border-slate-500 bg-gray-400 p-2">{props.children}</td>
+        <td className="border border-slate-500 p-2">{props.children}</td>
     )
 }
