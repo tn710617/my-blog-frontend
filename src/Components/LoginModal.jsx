93b7137852e995593f8ea0e95548ada8a91662b7
Ray <tn710617@gmail.com>
2023-06-12 07:49:37 +0800

    [feat] add multi lang support on login modal

--- src/Components/LoginModal.jsx
+++ src/Components/LoginModal.jsx
@@ -2,18 +2,21 @@ import React from "react";
 import Modal from "./Modal";
 import {useRecoilState} from "recoil";
 import {useLoginWithMetaMask} from "../APIs/auth";
+import {useIntl} from "react-intl";
 import loginModalAtom from "../States/loginModalAtom";
 
 export default function LoginModal() {
     const loginWithMetaMask = useLoginWithMetaMask()
+    const intl = useIntl()
     const [showLoginModal, setShowLoginModal] = useRecoilState(loginModalAtom)
     const handleGoBackButtonClick = async () => {
         await loginWithMetaMask.mutateAsync()
         setShowLoginModal(false)
     }
     return (
-        <Modal title={"請登入"} open={showLoginModal} handleGoBackButtonClick={handleGoBackButtonClick}
-               goBackButtonText={"請按此登入"} body={""}
+        <Modal title={intl.formatMessage({id: "login_modal.title"})} open={showLoginModal}
+               handleGoBackButtonClick={handleGoBackButtonClick}
+               goBackButtonText={intl.formatMessage({id: "login_modal.button_text"})} body={""}
                isLoading={loginWithMetaMask.isLoading}
                onHide={() => setShowLoginModal(false)}
                type={"info"}
