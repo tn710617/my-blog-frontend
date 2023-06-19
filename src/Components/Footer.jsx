a000eaa054b13296e2ae41493597de93f3f26c76
Ray <tn710617@gmail.com>
2023-06-19 19:48:52 +0800

    [fix] incorrect year in footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -7,7 +7,7 @@ export default function Footer()
         <footer>
             <div className={"h-20 bg-gray-800 flex gap-1 flex-col-reverse justify-center md:flex-row items-center md:justify-between px-12 py-8"}>
                 <div className={"text-gray-200 text-sm"}>
-                    © Copyright 2020-2022. All Rights Reserved.
+                    © Copyright 2016-{new Date().getFullYear()}. All Rights Reserved.
                 </div>
                 <div className={"flex gap-3 text-2xl text-gray-300"}>
                     <BsLinkedin className={"cursor-pointer"}/>
