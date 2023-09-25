06fadd5b7f1c61e5ecbf94ba65a6c0ea91d80a18
Ray <tn710617@gmail.com>
2023-09-25 18:50:07 +0800

    [feat] when ESC is pressed, close the searchBox

--- src/Components/Nav/SearchBoxComponent.jsx
+++ src/Components/Nav/SearchBoxComponent.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import SearchBoxInput from "./SearchBoxInput";
 
 export default function SearchBoxComponent({
@@ -12,9 +12,23 @@ export default function SearchBoxComponent({
         setShowSearchResultDropdown(false)
     }
 
+    useEffect(() => {
+        // when ESC key is pressed, close the search box
+        const handleEscKey = (event) => {
+            if (event.key === 'Escape') {
+                setShowSearchBoxComponent(false)
+                setShowSearchResultDropdown(false)
+            }
+        }
+        document.addEventListener('keydown', handleEscKey)
+        return () => {
+            document.removeEventListener('keydown', handleEscKey)
+        }
+    }, [setShowSearchBoxComponent, setShowSearchResultDropdown]);
+
     return (
         <div
-            className={`absolute left-0 top-0 w-screen h-screen backdrop-blur-sm z-10 ${showSearchBoxComponent ? '' : 'hidden'}`}
+            className={`absolute left-0 top-0 w-screen h-screen backdrop-blur-sm z-10 ${showSearchBoxComponent || 'hidden'}`}
             onClick={handleComponentClick}
         >
             <SearchBoxInput showSearchResultDropdown={showSearchResultDropdown}
