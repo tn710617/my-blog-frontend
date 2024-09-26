4022be1dfb351673bafa5a65f707a8d8c29f3d7e
Ray <tn710617@gmail.com>
2024-09-26 11:36:58 +0800

    [feat] improved work experience

--- src/Pages/About/WorkExperiencesEn.jsx
+++ src/Pages/About/WorkExperiencesEn.jsx
@@ -10,27 +10,55 @@ export function getWorkExperiencesEn() {
             "work_company_logo": SURASIALogo,
             "work_experience_description": (
                 <ul className={"list-disc list-outside ml-4"}>
-                    <li>Full-stack development of blockchain NFT trading market applications: front-end, back-end, smart
-                        contracts
+                    <li>Full-stack development of NFT marketplace application: Frontend, backend, and smart contracts
                     </li>
-                    <li>Cold wallet product: backend development</li>
-                    <li>Virtual currency card service: backend development</li>
-                    <li>Plan and implement a multi-version coexistence API architecture: provide support for at least
-                        two versions of the API when the Mobile APP is in updating process
+                    <li>Blockchain cold wallet: Backend development</li>
+                    <li>Blockchain payment card service: Backend development</li>
+                    <li>Planning and implementing API architecture for multi-version coexistence: Providing support for
+                        at least two API versions when the Mobile APP is under review for updates
                     </li>
-                    <li>Plan and implement HA architecture on products</li>
-                    <li>Complete the self-built blockchain node</li>
-                    <li>Implement VPN Server, hang VPN before ssh connection to enhance the company's project security
+                    <li>Planning and implementing product deployment architecture:
+                        <ul className={"list-disc list-outside ml-4"}>
+                            <li>Implementing HA architecture: <b>GCP LB</b> & <b>GCP Instance Group</b> & <b>Docker
+                                Swarm</b></li>
+                            <li>Building CI/CD pipeline to fit team operations: <b>Git
+                                Push</b> --> <b>Review</b> --> <b>One-click deployment</b></li>
+                            <li>Implementing scheduled health checks for various services, including but not limited
+                                to <b>APP Service</b>, <b>Web Service</b>, <b>DB</b>, <b>Redis</b>, <b>Node</b>, <b>VPN
+                                    Server</b></li>
+                            <li>Sending notifications to <b>Email</b> & <b>Slack channel</b> when any service health
+                                check fails
+                            </li>
+                            <li>Asynchronously streaming and centralizing every <b>request log</b> from
+                                different <b>containers</b> into a <b>Log Management Center</b> for future reference
+                            </li>
+                            <li>Sending <b>request log</b> & <b>error log</b> to <b>Slack</b> when unexpected errors
+                                occur
+                            </li>
+                            <li>For financial service security compliance, unifying the IP for all <b>outbound
+                                requests</b> to facilitate whitelist management by the financial service
+                            </li>
+                        </ul>
                     </li>
-                    <li>Product passed VAPT detection without any Medium or above threats</li>
-                    <li>Solve issue: The product uses CloudFlare SSL, causing the Response Time to be too long</li>
-                    <li>Solve issue: The product cannot run multiple nodes due to different MTUs in the Docker Swarm
-                        architecture
+                    <li>Blockchain self-hosted node</li>
+                    <li>Implementing <b>VPN Server</b>, attaching <b>VPN</b> before <b>ssh connection</b> to enhance
+                        security level of company projects
                     </li>
-                    <li>Solve issue: The response will be truncated when the response body exceeds the buffer length due
-                        to incorrect Nginx Buffer permission settings
+                    <li>Product passed <b>VAPT</b> testing with no threats rated Medium or above</li>
+                    <li>Issue resolved:
+                        <ul className={"list-disc list-outside ml-4"}>
+                            <li>Implemented <b>key rotation</b> mechanism to bypass third-party service <b>API rate
+                                limit</b></li>
+                            <li>Product response time was too long due to using <b>CloudFlare Proxy SSL</b></li>
+                            <li>Product's <b>Docker Swarm</b> architecture experienced communication failure between
+                                nodes due to differing <b>GCP MTU</b></li>
+                            <li>Response body was truncated when exceeding buffer length due to incorrect <b>Nginx
+                                Buffer</b> permissions settings
+                            </li>
+                        </ul>
                     </li>
                 </ul>
+
             )
         },
         {
--- src/Pages/About/WorkExperiencesZhTw.jsx
+++ src/Pages/About/WorkExperiencesZhTw.jsx
@@ -12,15 +12,34 @@ export function getWorkExperiencesZhTw() {
                 <ul className={"list-disc list-outside ml-4"}>
                     <li>NFT 交易市場應用全端開發：前端，後端，智能合約</li>
                     <li>區塊鏈冷錢包：後端開發</li>
-                    <li>虛擬貨幣發卡服務：後端開發</li>
-                    <li>規劃並實作多版本共存 API 架構：當 Mobile APP 在更新版本時，提供至少兩個版本的 API 支援</li>
-                    <li>規劃並且實作產品 HA 架構</li>
+                    <li>區塊鏈簽帳卡服務：後端開發</li>
+                    <li>規劃並實作多版本共存 API 架構：當 Mobile APP 在送審更新時，提供至少兩個版本的 API 支援</li>
+                    <li>規劃並且實作產品部署架構：
+                        <ul className={"list-disc list-outside ml-4"}>
+                            <li>實作 HA 架構：<b>GCP LB</b> & <b>GCP Instance Group</b> & <b>Docker Swarm</b></li>
+                            <li>建置符合團隊運作 CI/CD 管道：<b>Git Push</b> --> <b>審查</b> --> <b>一鍵部署</b></li>
+                            <li>針對各項服務實作固定頻率健康檢查，包含但不限於 <b>APP Service</b>，<b>Web
+                                Service</b>，<b>DB</b>，<b>Redis</b>，<b>Node</b>，<b>VPN Server</b></li>
+                            <li>任一服務健康檢查未通過時，發送通知到 <b>Email</b> & <b>Slack channel</b></li>
+                            <li>將不同 <b>container</b> 的每個 <b>request log</b> 非同步串流集中到 <b>Log 管理中心</b>，供後續查閱
+                            </li>
+                            <li>非預期 error 產生時，將 <b>request log</b> & <b>error log</b> 發送到 <b>Slack</b></li>
+                            <li>因應配合金融服務資安要求，將所有 <b>outbound request</b> 統一 IP，以利金融服務管理白名單
+                            </li>
+                        </ul>
+                    </li>
                     <li>區塊鏈自建節點</li>
-                    <li>實作 VPN Server，在 ssh connection 之前掛上 VPN，強化公司專案資安等級</li>
-                    <li>產品通過 VAPT 檢測，無任何 Medium 以上威脅</li>
-                    <li>解決 issue：產品因使用 CloudFlare SSL，造成 Response Time 過長</li>
-                    <li>解決 issue：產品在 Docker Swarm 的架構下，因 MTU 不同，導致無法運行 multiple node</li>
-                    <li>解決 issue：產品因 Nginx Buffer 權限設定不正確，當 Response Body 超出 Buffer 長度時，會被截斷</li>
+                    <li>實作 <b>VPN Server</b>，在 <b>ssh connection</b> 之前掛上 <b>VPN</b>，強化公司專案資安等級</li>
+                    <li>產品通過 <b>VAPT</b> 檢測，無任何 Medium 以上威脅</li>
+                    <li>Issue resolved：
+                        <ul className={"list-disc list-outside ml-4"}>
+                            <li>實作 <b>key rotation</b> 機制，突破第三方服務 <b>API rate limit</b></li>
+                            <li>產品因使用 <b>CloudFlare Proxy SSL</b>，造成 Response Time 過長</li>
+                            <li>產品在 <b>Docker Swarm</b> 的架構下，因 <b>GCP MTU</b> 不同，導致 Node 之間無法溝通</li>
+                            <li>產品因 <b>Nginx Buffer</b> 權限設定不正確，當 Response Body 超出 Buffer 長度時，會被截斷
+                            </li>
+                        </ul>
+                    </li>
                 </ul>
             )
         },
@@ -43,7 +62,9 @@ export function getWorkExperiencesZhTw() {
                     <li>Queue 進階應用：</li>
                     <ul className={"ml-4 list-disc list-outside list"}>
                         <li>隊列匯出<span className={"font-bold"}>數萬筆</span>的資料到 Excel，並上傳到 s3 供下載</li>
-                        <li>隊列發送<span className={"font-bold"}>十萬張</span>卷到<span className={"font-bold"}>五萬個</span> LINE 使用者</li>
+                        <li>隊列發送<span className={"font-bold"}>十萬張</span>卷到<span
+                            className={"font-bold"}>五萬個</span> LINE 使用者
+                        </li>
                     </ul>
                     <li>導入測試</li>
                     <li>主流第三方登入實作</li>
